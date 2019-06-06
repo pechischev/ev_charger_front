d994c8f0d9359e8295b5609432f62c646b7eb755
Maria Kanyushkova <mashakanyushkova@gmail.com>
2019-06-06 15:04:13 +0300

    [LEVC-95] fix amountField validate and parse functions

--- src/components/fields/AmountField.tsx
+++ src/components/fields/AmountField.tsx
@@ -4,7 +4,7 @@ import { IField } from "./IField";
 import "./Field.scss";
 import { InputField } from "./InputField";
 import { Nullable } from "@app/config";
-import { head } from "lodash";
+import { head, isNull } from "lodash";
 import { EMessages } from "@utils/EMessage";
 
 export const AmountField: FC<IField> = ({ name, ...rest }) => {
@@ -12,18 +12,20 @@ export const AmountField: FC<IField> = ({ name, ...rest }) => {
         if (!value) {
             return void 0;
         }
-        if (!/[a-zA-Z]/g.exec(value)) {
+
+        if (!isNull(value.match(/[a-zA-Z]/g)) || isNull(value.match(/\d+[.,]\d{2}/g))) {
             return EMessages.AMOUNT_INCORRECT;
         }
+
         return void 0;
     };
 
     const parseAmountValue = (value: string): Nullable<string> => {
-        if (!value) {
-            return void 0;
+        if (!validateAmountValue(value)) {
+            return head(/\d+[.,]\d{2}/g.exec(value) || []);
         }
 
-        return head(/\d+[.,]\d{2}/g.exec(value) || []);
+        return value;
     };
 
     return (
