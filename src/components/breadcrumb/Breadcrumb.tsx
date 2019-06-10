b2030f4161eae0a1acb8d14687b1e21b44dff1a0
pechischev_vladimir <vladimir.pechischev@omega-r.com>
2019-06-10 10:58:19 +0300

    [LEVC-101] update breadcrumb component

--- src/components/breadcrumb/Breadcrumb.tsx
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -6,21 +6,19 @@ import { IBreadcrumb, IBreadcrumbProps } from "./interfaces";
 
 export const Breadcrumb: FC<IBreadcrumbProps> = observer(({ crumbs = [] }) => {
     return (
-        <>
+        <div className="breadcrumb">
             {crumbs.map((item: IBreadcrumb, index) => {
                 return (
-                    <div className="breadcrumb" key={index}>
-                        <div
-                            className="breadcrumb_root"
-                            onClick={!!item.handler ? item.handler : void 0}
-                            data-active={!!item.handler}
-                        >
-                            {item.label}
-                        </div>
-                        <div className="breadcrumb_arrow" data-visible={!!item.handler}/>
+                    <div
+                        className="breadcrumb__root"
+                        onClick={item.handler}
+                        data-active={!!item.handler}
+                        key={index}
+                    >
+                        {item.label}
                     </div>
                 );
             })}
-        </>
+        </div>
     );
 });
