cca4c5722882d52ad78ff87a181d97fba8c131c9
Maria Kanyushkova <mashakanyushkova@gmail.com>
2019-06-14 15:41:59 +0300

    [LEVC-114] implement main card row in dashboard

--- src/app/screens/dashboard/Dashboard.tsx
+++ src/app/screens/dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Component, ReactNode } from "react";
+import { Card } from "@components/card";
+import "./Dashboard.scss";
 
 export class Dashboard extends Component {
     render(): ReactNode {
@@ -8,7 +10,54 @@ export class Dashboard extends Component {
                 <div className="page-header">
                     <div className="page-title">Dashboards</div>
                 </div>
-                <div className="page-content"/>
+                <div className="page-content">
+                    <div className="dashboard-main-cards">
+                        <Card
+                            className="main-cards"
+                            content={this.renderMainCard(
+                                "25",
+                                "Number of active residences",
+                                "residence",
+                            )}
+                        />
+                        <Card
+                            className="main-cards"
+                            content={this.renderMainCard(
+                                "25",
+                                "Number of active users",
+                                "users",
+                            )}
+                        />
+                        <Card
+                            className="main-cards"
+                            content={this.renderMainCard(
+                                "25",
+                                "Number of new users this month",
+                                "new-users",
+                            )}
+                        />
+                        <Card
+                            className="main-cards"
+                            content={this.renderMainCard(
+                                `${25}$`,
+                                "Total revenue this month",
+                                "revenue",
+                            )}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    private renderMainCard(value: string, title: string, icon: string): ReactNode {
+        return (
+            <div className="main-card">
+                <div className="main-card_description">
+                    <div className="main-card__title">{title}</div>
+                    <div className="main-card__value">{value}</div>
+                </div>
+                <div className="main-card_icon" data-image={icon}/>
             </div>
         );
     }
